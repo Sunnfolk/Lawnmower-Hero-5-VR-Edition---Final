2996ab59491a8713c2355927585973051646a2a7
TheSaltyGaming <38183890+TheSaltyGaming@users.noreply.github.com>
2022-07-30 20:52:14 +0200

    The start of what might become a scoreboard system

---
+++ Assets/Scripts/UI Scripts/ScoreboardLoader.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreboardLoader : MonoBehaviour
+{
+	#region Scoreboard Strings
+
+	[SerializeField] private string EasyOneScore;
+	[SerializeField] private string EasyTwoScore;
+	
+	[SerializeField] private string IntermediateOneScore;
+	[SerializeField] private string IntermediateTwoScore;
+	
+	[SerializeField] private string HardOneScore;
+	[SerializeField] private string HardTwoScore;
+
+	#endregion
+	
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void loadScore()
+    {
+	    
+    }
+	
+}
