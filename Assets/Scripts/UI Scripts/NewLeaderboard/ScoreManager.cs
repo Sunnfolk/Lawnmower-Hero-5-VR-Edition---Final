cf832486979af32768f65f295338187041de8a2a
TheSaltyGaming <38183890+TheSaltyGaming@users.noreply.github.com>
2022-09-08 17:21:59 +0200

    Working Leaderboard

---
+++ Assets/Scripts/UI Scripts/NewLeaderboard/ScoreManager.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using PlayFab.ExperimentationModels;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private ScoreData sd;
+
+    private void Awake()
+    {
+        //Uncomment this to delete all scores
+        //PlayerPrefs.DeleteKey("scores");
+        
+        var json = PlayerPrefs.GetString("scores", "{}");
+        //sd = new ScoreData();
+        sd = JsonUtility.FromJson<ScoreData>(json);
+        
+    }
+
+    public IEnumerable<Score> GetHighScores()
+    {
+        return sd.scores.OrderByDescending(x => x.score);
+    }
+
+    public void AddScore(Score score)
+    {
+        sd.scores.Add(score);
+    }
+
+    private void OnDestroy()
+    {
+        SaveScore();
+    }
+
+    public void SaveScore()
+    {
+        var json = JsonUtility.ToJson(sd);
+        PlayerPrefs.SetString("scores", json);
+    }
+}
